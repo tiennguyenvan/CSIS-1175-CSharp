c6320ac6bc608ae20e2f82ae7bec81a07b98755d
tiennguyenvan <nguyentien.jobs@gmail.com>
2024-02-06 12:02:55 -0800

    Change first guideline

--- Book/Book/Program.cs
+++ Book/Book/Program.cs
@@ -3,8 +3,7 @@ using static System.Console;
 namespace Program {
     class Program {
         static void Main(string[] args) {
-            int ans = 10, v1 = 5, v2 = 7, v3 = 18; ans += v1 + 10 * (v2 / 5) + v3 / v2;
-            WriteLine(ans);
+            WriteLine("Uncomment to test");
 
             //Chapter_1.Question_1.Solution();
             //Chapter_1.Question_2.Solution();
